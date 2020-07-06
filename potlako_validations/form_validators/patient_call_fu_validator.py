2f545862dd671ca6912a5c2a86e359c880b79f58
ame diphoko <diphokoame@gmail.com>
2020-07-06 09:16:28 +0200

    update patient call fu validations

--- potlako_validations/form_validators/patient_call_fu_validator.py
+++ potlako_validations/form_validators/patient_call_fu_validator.py
@@ -1,4 +1,4 @@
-from edc_constants.constants import NO, YES
+from edc_constants.constants import NO, YES, OTHER
 from edc_form_validators import FormValidator
 from .crf_form_validator import CRFFormValidator
 
@@ -29,11 +29,47 @@ class PatientCallFuFormValidator(CRFFormValidator, FormValidator):
                 field=field,
                 field_required=required_field)
 
+        for field in ['facility_visited_count', 'last_visit_date',
+                      'last_visit_date_estimated', 'last_visit_facility']:
+            self.required_if(
+                YES,
+                field='interval_visit',
+                field_required=field)
+
+        self.required_if(
+            YES,
+            field='last_visit_date_estimated',
+            field_required='last_visit_date_estimation')
+
+        self.required_if(
+            NO,
+            field='transport_support_received',
+            field_required='transport_details')
+
+        fields = {'clinician_communication_issues': 'clinician_issues_details',
+                  'communication_issues': 'issues_details',
+                  'other_issues': 'other_issues_details'}
+
+        for field, required_field in fields.items():
+            self.required_if(
+                YES,
+                field=field,
+                field_required=required_field)
+
         self.validate_other_specify(
             'next_ap_facility',
             other_specify_field='next_ap_facility_other',)
 
+        fields_other = ['appt_change_reason', 'sms_outcome']
+        for field_other in fields_other:
+            self.validate_other_specify(field=field_other)
+
+        self.m2m_other_specify(
+            OTHER,
+            m2m_field='call_achievements',
+            field_other='call_achievements_other')
+
         self.validate_next_appointment_date(
-            next_ap_date=self.cleaned_data.get('next_ap_date'))
+            next_ap_date=self.cleaned_data.get('next_appointment_date'))
 
         super().clean()
