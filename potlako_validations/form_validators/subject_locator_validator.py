62fdc692f7ad9aee43e8bc4062946f0bada5739f
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2020-08-18 15:45:24 +0200

    Refining validations

---
+++ potlako_validations/form_validators/subject_locator_validator.py
@@ -0,0 +1,9 @@
+from django.apps import apps as django_apps
+from django.core.exceptions import ValidationError
+from edc_form_validators import FormValidator
+
+
+class SubjectLocatorFormValodator(FormValidator):
+    pass
+
+
