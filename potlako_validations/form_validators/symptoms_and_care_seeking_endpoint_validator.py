7771a510380fbe03322279e2eb478af70d6a9ec8
samKenpachi011 <samuelkabelo1@gmail.com>
2024-03-22 09:56:36 +0200

    FIX: removed unused code

--- potlako_validations/form_validators/symptoms_and_care_seeking_endpoint_validator.py
+++ potlako_validations/form_validators/symptoms_and_care_seeking_endpoint_validator.py
@@ -1,6 +1,6 @@
 from django.apps import apps as django_apps
 from django.core.exceptions import ValidationError
-from edc_constants.constants import YES, NO
+from edc_constants.constants import YES
 from edc_form_validators import FormValidator
 
 
